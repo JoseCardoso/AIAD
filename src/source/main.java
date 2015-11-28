8200792f85c789769342edaa7cc7e10afa071f39
Diogoei12102 <ei12102@fe.up.pt>
2015-11-28 19:08:40 +0000

    map added

--- src/source/main.java
+++ src/source/main.java
@@ -1,54 +1,76 @@
 package source;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import agents.SemaphoreAgent;
+import agents.SemaphoreManager;
+import trasmapi.genAPI.Simulator;
 import trasmapi.genAPI.TraSMAPI;
 import trasmapi.genAPI.exceptions.TimeoutException;
 import trasmapi.genAPI.exceptions.UnimplementedMethod;
 import trasmapi.sumo.Sumo;
-import trasmapi.sumo.SumoCom;
 
-public class main {
-	static public void main(String[] Args) {
-		// Sumo initialization.
-		Sumo sumo = new Sumo("guisim");
+import jade.BootProfileImpl;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+import start.ODManager;
+
+public class Main {
+	
+	static boolean JADE_GUI = true;
+	private static ProfileImpl profile;
+	private static ContainerController mainContainer;
+	
+	public static void main(String[] args) throws UnimplementedMethod, InterruptedException, IOException, TimeoutException {	
+
+		//Init JADE platform w/ or w/out GUI
+		if(JADE_GUI){
+			List<String> params = new ArrayList<String>();
+			params.add("-gui");
+			profile = new BootProfileImpl(params.toArray(new String[0]));
+		} else
+			profile = new ProfileImpl();
+
+		Runtime rt = Runtime.instance();
+		
+		//mainContainer - agents' container
+		mainContainer = rt.createMainContainer(profile);
+		
+		TraSMAPI api = new TraSMAPI(); 
+
+		//Create SUMO
+		Simulator sumo = new Sumo("guisim");
 		List<String> params = new ArrayList<String>();
-		params.add("-c=TlMap/map.sumo.cfg");
-		sumo.addConnections("localhost", 4040);
-
-		// TraSMAPI initialization.
-		TraSMAPI api = new TraSMAPI();
+		// get current path
+		Path currentRelativePath = Paths.get("");
+		String current_path = currentRelativePath.toAbsolutePath().toString();
+		//add to params
+		params.add("-c="+current_path +"/bin/Maps/cross1ltl/cross1ltl.sumocfg");
+		sumo.addParameters(params);
+		
+		sumo.addConnections("127.0.0.1", 8870);
+		
+		//Add Sumo to TraSMAPI
 		api.addSimulator(sumo);
-		try {
-			api.launch();
-			api.connect();
-			api.start();
+		
+		//Launch and Connect all the simulators added
+		api.launch();
 
+		api.connect();
 
-			while(true)
-			{
-				int currentStep = SumoCom.getCurrentSimStep();
-				if(!api.simulationStep(0))
-					break;
-			}
-			api.close();
-		}
-		catch(IOException|
-
-				UnimplementedMethod e)
-
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch(
-
-				TimeoutException e)
-
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+		SemaphoreManager manager = new SemaphoreManager(mainContainer);
+		
+		
+		api.start();
+		//simulation loop
+		while(true)
+			if(!api.simulationStep(0))
+				break;
+	}	
 }
