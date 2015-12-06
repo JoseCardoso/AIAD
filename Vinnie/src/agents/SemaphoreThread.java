055d02d1a33ba1260d7e4aa0710e55f0d9b27576
Diogoei12102 <ei12102@fe.up.pt>
2015-12-06 23:36:19 +0000

    Semaphore alternating from time to time

--- Vinnie/src/agents/SemaphoreThread.java
+++ Vinnie/src/agents/SemaphoreThread.java
@@ -3,28 +3,39 @@ package agents;
 import trasmapi.sumo.Sumo;
 import trasmapi.sumo.SumoTrafficLight;
 
-public class SemaphoreThread implements Runnable{
-	
+public class SemaphoreThread implements Runnable {
+
 	private Sumo sumo;
 	private SemaphoreAgent agent;
-	
-	
-	
+
 	public SemaphoreThread(Sumo sumo, SemaphoreAgent agent) {
 		this.sumo = sumo;
 		this.agent = agent;
 	}
 
-
-
-	public void run()
-	{
+	public void run() {
+		boolean position = true;
 		SumoTrafficLight semaphore = new SumoTrafficLight(agent.ID);
-		while(true)
-		{
-			//states of all semaphores
-			semaphore.setState("rGyrGryGryGryGyrGrrrrrrrrrrrrGGGGGGGGGGGyyyGGGGGGGGGGGGGGGGGGGGGGGG");
-			
+		while (true) {
+			// states of all semaphores
+			if (agent.getAdjacents().size() == 4 && position) {
+				semaphore.setState("rrrgGGrrrgGG");
+				position = false;
+			} else if (agent.getAdjacents().size() == 4 && !position) {
+				semaphore.setState("gGGrrrgGGrrr");
+				position = true;
+			} else if (agent.getAdjacents().size() == 3 && position) {
+				semaphore.setState("rrgGrr");
+			}
+			else if (agent.getAdjacents().size() == 3 && !position) {
+				semaphore.setState("gGrrgG");
+			}
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
