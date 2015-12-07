909761e437dff85ace499a2d714a316f9120da81
Diogoei12102 <ei12102@fe.up.pt>
2015-12-07 18:23:56 +0000

    change folder name

---
+++ Lights/src/agents/AgentManager.java
@@ -0,0 +1,73 @@
+package agents;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+
+
+import jade.wrapper.ContainerController;
+import jade.wrapper.ControllerException;
+import jade.wrapper.StaleProxyException;
+import trasmapi.sumo.Sumo;
+import trasmapi.sumo.SumoCom;
+import trasmapi.sumo.SumoTrafficLight;
+
+public class AgentManager {
+	Sumo sumo;
+	ContainerController mainContainer;
+	private final int numDrivers = 25;
+	ArrayList<SemaphoreAgent> agents = new ArrayList<>();
+	
+	public AgentManager(Sumo sumo, ContainerController mainContainer) {
+		this.sumo = sumo;
+		this.mainContainer = mainContainer;
+	}
+
+	public void initSemaphores() {
+		ArrayList<String> semaphoreIDS = SumoTrafficLight.getIdList();
+		for (int i = 0; i < semaphoreIDS.size(); i++) {
+			SumoTrafficLight semaphore = new SumoTrafficLight(semaphoreIDS.get(i));
+			ArrayList<String> controlledLanes = semaphore.getControlledLanes();
+			HashSet<String>  adjacentSemaphores= new HashSet<String>();
+
+
+			for(int j = 0; j < controlledLanes.size(); j++)
+			{
+				String neighbour= controlledLanes.listIterator(j).next().split("to")[0];
+				adjacentSemaphores.add(neighbour);
+			}
+			SemaphoreAgent agent = new SemaphoreAgent(sumo, semaphoreIDS.get(i));
+			HashSet<String> agentSet = new HashSet<String>();
+			agentSet.addAll(adjacentSemaphores);
+			
+			agent.setAdjacents(adjacentSemaphores);
+			
+				try {
+				agents.add(agent);
+				mainContainer.acceptNewAgent("Semaphore-" + semaphoreIDS.get(i), agent);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+
+
+		}
+	}
+
+	public void startSemaphores()
+	{
+		for(int i = 0; i < agents.size();i++)
+		{
+			try {
+				mainContainer.getAgent(agents.listIterator(i).next().getLocalName()).start();
+			} catch (StaleProxyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ControllerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+
+}
--- Lights/src/agents/SemaphoreAgent.java
+++ Lights/src/agents/SemaphoreAgent.java
@@ -4,7 +4,6 @@ package agents;
 import java.util.HashSet;
 
 import trasmapi.sumo.Sumo;
-import trasmapi.sumo.SumoTrafficLight;
 
 public class SemaphoreAgent extends jade.core.Agent{
 	/**
@@ -21,10 +20,10 @@ public class SemaphoreAgent extends jade.core.Agent{
 		ID = string;
 		this.sumo = sumo;
 		
+		new Thread(new SemaphoreThread(sumo, this)).start();
 	}
 
 	public void setup() {
-		executeSemaphore();
 		// esta funo inicializa o semaforo
 		//	System.out.println("SEMAFORO INICIALIZADO  " + ID);
 	}
@@ -36,108 +35,7 @@ public class SemaphoreAgent extends jade.core.Agent{
 	public void setAdjacents(HashSet<String> adjacents) {
 		this.adjacents = adjacents;
 	}
-	public void executeSemaphore() {
-		boolean position = true, yellow = false;
-		SumoTrafficLight semaphore = new SumoTrafficLight(ID);
-		int i = 0;
-		while (true) {
-			// states of all semaphores
-			if (getAdjacents().size() == 4 && position) {
-				if (!yellow)
-					semaphore.setState("rrrGGgrrrgGG");
-				else
-					semaphore.setState("rrryyyrrryyy");
-			} else if (getAdjacents().size() == 4 && !position) {
-				if (!yellow)
-					semaphore.setState("GGgrrrGGgrrr");
-				else
-					semaphore.setState("yyyrrryyyrrr");
-			} else if (getAdjacents().size() == 3) {
 
-				semaphore.setState(generateState(position, yellow));
 
-			}
-			try {
-				Thread.sleep(2);
-				i++;
-				if (i > 55) {
-					i = 0;
-					position = true;
-					yellow = false;
-				} else if (i > 50) {
-					yellow = true;
-				} else if (i > 25) {
-					position = false;
-
-					yellow = false;
-				} else if (i > 20) {
-
-					yellow = true;
-
-				}
-
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-
-	private String generateState(boolean position, boolean yellow) {
-		String Str = "";
-		int column = Integer.parseInt(ID.split("/")[0]);
-		int line = Integer.parseInt(ID.split("/")[1]);
-		String upper = Integer.toString(column) + "/" + Integer.toString(line + 1);
-		String righter = Integer.toString(column + 1) + "/" + Integer.toString(line);
-		String below = Integer.toString(column) + "/" + Integer.toString(line - 1);
-		String lefter = Integer.toString(column - 1) + "/" + Integer.toString(line);
-		if (position) {
-			if (getAdjacents().contains(upper)) {
-				if (!yellow)
-					Str+="Gg";
-				else
-					Str+="yy";
-			}
-			if (getAdjacents().contains(righter)) {
-				Str+="rr";
-			}
-			if (getAdjacents().contains(below)) {
-				if (!yellow)
-					Str+="gG";
-				else
-					Str+="yy";
-
-			}
-			if (getAdjacents().contains(lefter)) {
-				Str+="rr";
-			}
-		} else {
-			if (getAdjacents().contains(upper)) {
-				Str+="rr";
-
-			}
-			if (getAdjacents().contains(righter)) {
-				if (!yellow)
-					Str+="Gg";
-				else
-					Str+="yy";
-			}
-			if (getAdjacents().contains(below)) {
-
-				Str+="rr";
-
-			}
-			if (getAdjacents().contains(lefter)) {
-				if (!yellow)
-					Str+="gG";
-				else
-					Str+="yy";
-			}
-		}
-
-		return Str.toString();
-	}
 
 }
-
-
